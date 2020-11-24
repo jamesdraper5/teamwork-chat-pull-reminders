e9711a20f6c0885f7cc2b084da3b5db35e42526f
James Draper <jamesdrape@gmail.com>
2020-11-24 16:52:43 +0000

    Allow multiple announcements

--- handler.js
+++ handler.js
@@ -5,9 +5,38 @@ const getStalePRs = require('./lib/github');
 const sendMessage = require('./lib/sendMessage');
 
 const channelUrl = process.env.CHANNEL_URL;
+const relevantRepos = process.env.REPO_NAMES.split(',');
 
-async function getMessageText() {
-  const stalePRs = await getStalePRs();
+const teams = [
+  {
+    name: 'Echo',
+    channelUrl: process.env.CHANNEL_URL,
+    repos: relevantRepos,
+    opts: {} // you can add a custom `filterFn` here to filter the PRs
+  },
+  {
+    name: 'Reports Channel',
+    channelUrl: 'https://chat-hooks.us.teamwork.com/v1/in/1/467af93b-10f2-4338-ae7b-da5abf6ce742',
+    repos: [
+      'project-manager',
+      'projects-web-app'
+    ],
+    opts: {
+      filterFn: (pr) => pr.node.isDraft
+    }
+  },
+  {
+    name: 'Custom Fields',
+    channelUrl: 'https://chat-hooks.us.teamwork.com/v1/in/1/52ed7609-7161-460f-8745-d4b319e6eec1',
+    repos: [
+      'projectsapigo'
+    ],
+    opts: {}
+  }
+];
+
+async function getMessageText(repos, opts = {}) {
+  const stalePRs = await getStalePRs(repos, opts);
   console.log("stalePRs", stalePRs);
   const formattedPRs = stalePRs.map(formatPR);
   console.log("formattedPRs", formattedPRs);
@@ -33,9 +62,13 @@ function getDaysAgo(date) {
 
 module.exports.sendUpdate = async event => {
   try {
-    const messageText = await getMessageText();
-    console.log('messageText', messageText)
-    await sendMessage(messageText, channelUrl);
+    await Promise.all(
+      teams.map(async team => {
+        const messageText = await getMessageText(team.repos, team.opts);
+        console.log('messageText', messageText)
+        await sendMessage(messageText, channelUrl);
+      })
+    );
     return {
       statusCode: 200,
       body: JSON.stringify(
