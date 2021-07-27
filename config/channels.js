4ea8faba43d4196ce57ec51e61a5324bf1a62d71
James Draper <jamesdrape@gmail.com>
2021-07-27 16:45:42 +0100

    Filter commits by Bravo team members

--- config/channels.js
+++ config/channels.js
@@ -1,3 +1,13 @@
+const bravoTeamMembers = [
+  'jamesdraper5',
+  'gkubisa',
+  'synapse',
+  'atilkan',
+  'visaran',
+  'sineadcullinane',
+  'EmilyLawlor'
+];
+
 /*
 available opts:
   - filterFn: A custom filter function to filter the PRs, e.g. by draft only: `(pr) => pr.node.isDraft` - the available nodes can been seen in the query in github.js
@@ -5,12 +15,19 @@ available opts:
 */
 module.exports = [
   {
-    name: 'Team Echo',
+    name: 'Bravo Team',
     url: 'https://chat-hooks.us.teamwork.com/v1/in/1/2663f77d-9477-4a10-a868-2c5ee06661a7',
     repos: [
       'project-manager',
       'projects-web-app'
     ],
-    opts: {}
+    opts: {
+      filterFn: function(pr) {
+        return !pr.node.isDraft &&
+          pr.node.reviews.nodes.length === 0 &&
+          pr.node.comments.nodes.length === 0 &&
+          bravoTeamMembers.includes(pr.node.author.login);
+      }
+    }
   }
 ];
