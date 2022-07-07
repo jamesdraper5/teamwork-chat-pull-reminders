318ec7c9660c69f36698577e3fb66f627c1eb990
James Draper <jamesdrape@gmail.com>
2022-07-07 22:15:32 +0100

    Return early if no stale PRs

--- config/channels.js
+++ config/channels.js
@@ -17,7 +17,7 @@ module.exports = [
   {
     name: "Bravo Team",
     url: "https://chat-hooks.us.teamwork.com/v1/in/1/2663f77d-9477-4a10-a868-2c5ee06661a7",
-    repos: ["project-manager", "teamwork-web-app"],
+    repos: ["teamwork-lightspeed", "teamwork-web-app"],
     opts: {
       filterFn: function (pr) {
         return (
--- handler.js
+++ handler.js
@@ -1,63 +1,69 @@
-'use strict';
+"use strict";
 
-const moment = require('moment');
-const getStalePRs = require('./lib/github');
-const sendMessage = require('./lib/sendMessage');
-const channels = require('./config/channels');
+const moment = require("moment");
+const getStalePRs = require("./lib/github");
+const sendMessage = require("./lib/sendMessage");
+const channels = require("./config/channels");
 
-async function getMessageText(repos, opts = {}) {
-  const stalePRs = await getStalePRs(repos, opts);
-  console.log("stalePRs", stalePRs);
+async function getMessageText(stalePRs) {
   const formattedPRs = stalePRs.map(formatPR);
   console.log("formattedPRs", formattedPRs);
-  let messageText = opts.message || `There are ${stalePRs.length} PRs with no reviews or comments:`;
+  let messageText = `There are ${stalePRs.length} PRs with no reviews or comments:`;
   messageText += `\n- ${formattedPRs.join("\n-")}`;
   console.log("messageText", messageText);
   return messageText;
 }
 
 function formatPR(pr) {
-  return `[${pr.repo}] - [${pr.title}](${pr.url}) - ${pr.author} (${getDaysAgo(pr.createdAt)})`;
+  return `[${pr.repo}] - [${pr.title}](${pr.url}) - ${pr.author} (${getDaysAgo(
+    pr.createdAt
+  )})`;
 }
 
 function getDaysAgo(date) {
-  const daysOld = moment().startOf('day').diff(moment(date).startOf('day'), "days");
+  const daysOld = moment()
+    .startOf("day")
+    .diff(moment(date).startOf("day"), "days");
   if (daysOld === 1) {
     return `${daysOld} day old`;
   }
   if (daysOld === 0) {
-    return 'Created Today'
+    return "Created Today";
   }
   return `${daysOld} days old`;
 }
 
-module.exports.sendUpdate = async event => {
+module.exports.sendUpdate = async (event) => {
   try {
     await Promise.all(
-      channels.map(async channel => {
-        const messageText = await getMessageText(channel.repos, channel.opts);
-        console.log('messageText', messageText);
-        await sendMessage(messageText, channel.url);
+      channels.map(async (channel) => {
+        const stalePRs = await getStalePRs(channel.repos, channel.opts);
+        console.log("stalePRs", stalePRs);
+        if (stalePRs.length) {
+          const messageText = await getMessageText(stalePRs);
+          console.log("messageText", messageText);
+          await sendMessage(messageText, channel.url);
+        }
       })
     );
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: 'Function executed successfully!',
+          message: "Function executed successfully!",
           input: event,
         },
         null,
         2
       ),
     };
-  } catch(e) {
+  } catch (e) {
     return {
       statusCode: 500,
       body: JSON.stringify(
         {
-          message: 'Error',
-          error: e
+          message: "Error",
+          error: e,
         },
         null,
         2
