4629d664e8872cc2587d5317fad16b3d8760b770
James Draper <jamesdrape@gmail.com>
2022-09-16 17:27:59 +0100

    return value

--- config/channels.js
+++ config/channels.js
@@ -12,9 +12,12 @@ const bravoTeamMembers = [
 function getDiffInDays(date1, date2) {
   const diffTime = Math.abs(date2 - date1);
   const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
 }
 
 function getDaysSinceToday(date) {
+  console.log("getDaysSinceToday", date);
+
   return getDiffInDays(date, new Date());
 }
 
